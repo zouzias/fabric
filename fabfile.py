c23c337dce6c43293f75e450e72c7bf332585ef7
Anastasios Zouzias <zouzias@gmail.com>
2015-04-10 10:33:04 +0200

    Add more recipes

--- fabfile.py
+++ fabfile.py
@@ -3,4 +3,14 @@ from fabric.api import *
 def host_type():
     run('uname -s')
     
-  # TODO
+def apt_update():
+    sudo('apt-get update')
+
+def apt_upgrade():
+    sudo('apt-get -qy upgrade')
+
+def uptime():
+    run('uptime')
+
+def reboot():
+    sudo('reboot')
